7824d997e92300175b706054db7b6cfa90234c52
hamzaian <34007006+hamzaian@users.noreply.github.com>
2020-04-28 16:24:27 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links Hamza:)</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <li><a href="Last_AboutMe.html">AboutMe.html</a></li></br>
+      <li><a href="Last_Projects.html">Projects.html</a></li></br>
+      <li><a href="Last_Resume.html">Resume.html</a></li></br>
+      <li><a href="Last_ContactMe.html">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
